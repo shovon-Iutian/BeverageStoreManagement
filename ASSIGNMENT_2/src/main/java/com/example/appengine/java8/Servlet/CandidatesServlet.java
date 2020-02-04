28bdff395fff6bb9eb955c0cfdbda8f90619aa54
Mohammed Mehedi Hasan <mohammed-mehedi.hasan@stud.uni-bamberg.de>
2020-02-04 06:48:34 +0100

    - Implement servlet and jsp class for candidates creation, modification and deletion.

--- ASSIGNMENT_2/src/main/java/com/example/appengine/java8/Servlet/CandidatesServlet.java
+++ ASSIGNMENT_2/src/main/java/com/example/appengine/java8/Servlet/CandidatesServlet.java
@@ -4,7 +4,7 @@ import com.example.appengine.java8.DTO.Candidates;
 import com.example.appengine.java8.Entity.CandidatesEntity;
 import com.example.appengine.java8.Management.CandidatesManagement;
 import com.example.appengine.java8.Service.CandidateManagementService;
-import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,61 +15,64 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
-@WebServlet(name = "Candidates", value = "/candidates")
+@WebServlet(name = "Candidates", value = "/admin/candidates")
 public class CandidatesServlet extends HttpServlet {
     private static Logger logger = Logger.getLogger(CandidatesServlet.class.getName());
 
-    private Candidates candidates = new Candidates();
     private CandidateManagementService<Candidates> candidateManagementService = new CandidatesManagement();
     private CandidatesEntity candidatesEntity = new CandidatesEntity();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Candidates "creation" mechanism
-        candidates.setKey(candidatesEntity.getCandidateKey());
-        candidates.setFirstName("Mehedi");
-        candidates.setSurName("Shovon");
-        candidates.setFaculty("CSE");
-        candidates = candidateManagementService.create(candidates);
-        candidates.setKey(candidatesEntity.getCandidateKey());
-        candidates.setFirstName("Mehedi");
-        candidates.setSurName("Shovon");
-        candidates.setFaculty("SWT");
-        candidates = candidateManagementService.create(candidates);
-
-        // Candidates "deletion" mechanism
-        Query.Filter newFilter = new Query.FilterPredicate("Faculty", Query.FilterOperator.EQUAL, "SWT");
-        Query filterQuery = new Query(candidatesEntity.getCandidateKind()).setFilter(newFilter);
-        candidateManagementService.delete(filterQuery);
-
-        // Candidates "update" mechanism
-        newFilter = new Query.FilterPredicate("Faculty", Query.FilterOperator.EQUAL, "CSE");
-        filterQuery = new Query(candidatesEntity.getCandidateKind()).setFilter(newFilter);
-        List<Candidates> specificCandidatesList = candidateManagementService.get(filterQuery);
-        for (Candidates specificCandidate : specificCandidatesList) {
-            candidates.setKey(specificCandidate.getKey());
-            candidates.setFaculty("ISSYSC");
-            candidates = candidateManagementService.update(candidates);
+        Candidates candidates = new Candidates();
+        try {
+            Query query = new Query(candidatesEntity.getCandidateKind());
+            List<Candidates> candidatesList = candidateManagementService.get(query);
+            if (candidatesList != null) req.getSession().setAttribute("candidates", candidatesList);
+            req.getRequestDispatcher("/candidatesManagementForm.jsp").forward(req, resp);
+        } catch (Exception e){
+            logger.severe("Unable to retrieve candidates " + e.getMessage());
+            resp.getWriter().write(e.getMessage());
         }
-
-        // Candidates "getall" mechanism
-        Query query = new Query(candidatesEntity.getCandidateKind());
-        List<Candidates> candidatesList = candidateManagementService.get(query);
-        if(candidatesList != null) req.getSession().setAttribute("candidates", candidatesList);
-        req.getRequestDispatcher("/candidatesManagementForm.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Candidates candidates = new Candidates();
-        candidates.setFirstName("Mehedi");
-        candidates.setSurName("Shovon");
-        candidates.setFaculty("CSE");
-        candidates = candidateManagementService.create(candidates);
+        try {
+            String id = req.getParameter("id");
+            String firstName = req.getParameter("firstname");
+            String surName = req.getParameter("surname");
+            String faculty = req.getParameter("faculty");
+            System.out.println(id + " " + firstName + " " + " " + surName + " " + faculty);
+            candidates.setFirstName(firstName);
+            candidates.setSurName(surName);
+            candidates.setFaculty(faculty);
+            if(id == null) {
+                candidates.setKey(candidatesEntity.getCandidateKey());
+                candidates = candidateManagementService.create(candidates);
+            }
+            else {
+                candidates.setId(Long.valueOf(id));
+                candidates = candidateManagementService.update(candidates);
+            }
+            resp.sendRedirect(req.getContextPath() + "/admin/candidates");
+        } catch (Exception e){
+            logger.severe("Unable to create or update a candidate " + e.getMessage());
+            resp.getWriter().write(e.getMessage());
+        }
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
+        Candidates candidates = new Candidates();
+        try {
+            String id = req.getParameter("id");
+            candidates.setId(Long.valueOf(id));
+            candidateManagementService.delete(candidates);
+        } catch (Exception e){
+            logger.severe("Unable to delete a candidate " + e.getMessage());
+            resp.getWriter().write(e.getMessage());
+        }
     }
 }
