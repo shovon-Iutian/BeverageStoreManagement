32536f5e6bcf9e0b2784c620d14fbc6e67cd2c92
Istiaq <ihistiaqhasan05@gmail.com>
2020-02-05 00:08:19 +0100

    -voting time feature completed -voting result servlet updated

--- ASSIGNMENT_2/src/main/java/com/example/appengine/java8/Management/VoteTimeManagement.java
+++ ASSIGNMENT_2/src/main/java/com/example/appengine/java8/Management/VoteTimeManagement.java
@@ -3,7 +3,10 @@ package com.example.appengine.java8.Management;
 import com.example.appengine.java8.DTO.VoteTime;
 import com.example.appengine.java8.Entity.VoteTimeEntity;
 import com.example.appengine.java8.Service.VoteTimeManagementService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
 
 import java.util.Date;
 
@@ -12,19 +15,25 @@ public class VoteTimeManagement extends AbstractCrudManagement<VoteTime> impleme
     private VoteTimeEntity voteTimeEntity = new VoteTimeEntity();
 
     @Override
-    protected Entity convertDtoToEntity(VoteTime voteTime) {
-        Entity voteEntity= new Entity(voteTimeEntity.getVoteTimeKind(),voteTimeEntity.getVoteTimeKey());
-        voteEntity.setProperty(voteTimeEntity.getVoteTimeFirstProperty(), voteTime.getStartDate());
-        voteEntity.setProperty(voteTimeEntity.getVoteTimeSecondProperty(), voteTime.getEndDate());
-        return voteEntity;
+    protected Entity convertDtoToEntity(VoteTime voteTime)  {
+        try {
+            Entity voteEntity= voteTime.getId()== null ?  new Entity(voteTimeEntity.getVoteTimeKind(),voteTimeEntity.getVoteTimeKey()):
+                    DatastoreServiceFactory.getDatastoreService().get(KeyFactory.createKey(voteTimeEntity.getVoteTimeKey(), voteTimeEntity.getVoteTimeKind(), voteTime.getId()));
+            voteEntity.setProperty(voteTimeEntity.getVoteTimeFirstProperty(), voteTime.getStartdate());
+            voteEntity.setProperty(voteTimeEntity.getVoteTimeSecondProperty(), voteTime.getEnddate());
+            return voteEntity;
+        } catch (EntityNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     protected VoteTime convertEntityToDto(Entity entity) {
         VoteTime voteTime = new VoteTime();
         voteTime.setKey(entity.getKey());
-        voteTime.setStartDate((Date) entity.getProperty(voteTimeEntity.getVoteTimeFirstProperty()));
-        voteTime.setEndDate((Date) entity.getProperty(voteTimeEntity.getVoteTimeSecondProperty()));
+        voteTime.setStartdate((Date) entity.getProperty(voteTimeEntity.getVoteTimeFirstProperty()));
+        voteTime.setEnddate((Date) entity.getProperty(voteTimeEntity.getVoteTimeSecondProperty()));
         return voteTime;
     }
 }
